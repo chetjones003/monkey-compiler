d1d9ead054c106a2ba2501d8e14cfaddf7c96ba0
Chet <chetjones003@gmail.com>
2023-06-02 04:55:43 -0500

    add repl

--- go/lexer/lexer_test.go
+++ go/lexer/lexer_test.go
@@ -109,3 +109,77 @@ let result = add(five, ten);
 
 	}
 }
+
+func TestNextToken3(t *testing.T) {
+  input := `!-/*5;
+5 < 10 > 5;
+
+if (5 < 10) {
+  return true;
+} else {
+  return false;
+}
+
+10 == 10;
+10 != 9;
+`
+  
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+    {token.BANG, "!"},
+    {token.MINUS, "-"},
+    {token.SLASH, "/"},
+    {token.ASTERISK, "*"},
+    {token.INT, "5"},
+    {token.SEMICOLON, ";"},
+    {token.INT, "5"},
+    {token.LT, "<"},
+    {token.INT, "10"},
+    {token.GT, ">"},
+    {token.INT, "5"},
+    {token.SEMICOLON, ";"},
+    {token.IF, "if"},
+    {token.LPAREN, "("},
+    {token.INT, "5"},
+    {token.LT, "<"},
+    {token.INT, "10"},
+    {token.RPAREN, ")"},
+    {token.LBRACE, "{"},
+    {token.RETURN, "return"},
+    {token.TRUE, "true"},
+    {token.SEMICOLON, ";"},
+    {token.RBRACE, "}"},
+    {token.ELSE, "else"},
+    {token.LBRACE, "{"},
+    {token.RETURN, "return"},
+    {token.FALSE, "false"},
+    {token.SEMICOLON, ";"},
+    {token.RBRACE, "}"},
+    {token.INT, "10"},
+    {token.EQUAL, "=="},
+    {token.INT, "10"},
+    {token.SEMICOLON, ";"},
+    {token.INT, "10"},
+    {token.NOTEQUAL, "!="},
+    {token.INT, "9"},
+    {token.SEMICOLON, ";"},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+
+	}
+}
--- go/token/token.go
+++ go/token/token.go
@@ -8,30 +8,48 @@ type Token struct {
 }
 
 const (
-	ILLEGAL  = "ILLEGAL"
-	EOF      = "EOF"
-	IDENT    = "IDENT"
-	INT      = "INT"
-	ASSIGN   = "="
-	PLUS     = "+"
-	COMMA    = ","
+	ILLEGAL   = "ILLEGAL"
+	EOF       = "EOF"
+	IDENT     = "IDENT"
+	INT       = "INT"
+	ASSIGN    = "="
+	EQUAL     = "=="
+	NOTEQUAL  = "!="
+	PLUS      = "+"
+	MINUS     = "-"
+	BANG      = "!"
+	ASTERISK  = "*"
+	SLASH     = "/"
+	LT        = "<"
+	GT        = ">"
+	COMMA     = ","
 	SEMICOLON = ";"
-	LPAREN   = "("
-	RPAREN   = ")"
-	LBRACE   = "{"
-	RBRACE   = "}"
-	FUNCTION = "FUNCTION"
-	LET      = "LET"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	FUNCTION  = "FUNCTION"
+	LET       = "LET"
+	TRUE      = "TRUE"
+	FALSE     = "FALSE"
+	IF        = "IF"
+	ELSE      = "ELSE"
+	RETURN    = "RETURN"
 )
 
-var keywords = map[string]TokenType {
-  "fn": FUNCTION,
-  "let": LET,
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
-  if tok, ok := keywords[ident]; ok {
-    return tok
-  }
-  return IDENT
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
 }
